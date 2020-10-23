cdce03df7cbafe7f23bf7442fa906c60f53b514c
cray650 <69844416+cray650@users.noreply.github.com>
2020-10-23 16:52:28 +0900

    勤務時間保存機能実装

--- app/controllers/time_cards_controller.rb
+++ app/controllers/time_cards_controller.rb
@@ -6,10 +6,24 @@ class TimeCardsController < ApplicationController
   end
 
   def new
+    @year = Time.current.year
+    @month = Time.current.month
+    @day = Time.current.day
     @time_card = TimeCard.new
+
   end
 
   def create
-    @time_card = TimeCard.new
+    @time_card = TimeCard.new(time_card_params)
+    if @time_card.save
+      render :index
+    else
+      render :new
+    end
+  end
+
+  private
+  def time_card_params
+    params.require(:time_card).permit(:year, :month, :day, :in, :out).merge(user_id: current_user.id)
   end
 end
--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,3 +1,12 @@
 class TimeCard < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do 
+    validates :year
+    validates :month
+    validates :in
+    validates :out
+  end
+
+
 end
