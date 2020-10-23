335c60da6ed13bdb3980886aecf1059818850ac2
cray650 <69844416+cray650@users.noreply.github.com>
2020-10-23 15:34:07 +0900

    出勤画面実装

--- app/controllers/time_cards_controller.rb
+++ app/controllers/time_cards_controller.rb
@@ -1,5 +1,15 @@
 class TimeCardsController < ApplicationController
   
   def index
+    @year = Time.current.year
+    @month = Time.current.month
+  end
+
+  def new
+    @time_card = TimeCard.new
+  end
+
+  def create
+    @time_card = TimeCard.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'time_cards#index'
+  root to: 'time_cards#new'
+  resources  :time_cards
 end
