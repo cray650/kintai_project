8228d4408e14aead6b6328e195072eb1f0678c94
cray650 <69844416+cray650@users.noreply.github.com>
2020-10-25 16:02:52 +0900

    gem rails_12factorの追加

--- app/controllers/time_cards_controller.rb
+++ app/controllers/time_cards_controller.rb
@@ -1,4 +1,32 @@
 class TimeCardsController < ApplicationController
   def index
+<<<<<<< Updated upstream
+=======
+    @year = Time.current.year
+    @month = Time.current.month
+    @day = Time.current.day
+  end
+
+  def new
+    @year = Time.current.year
+    @month = Time.current.month
+    @day = Time.current.day
+    @time = Time.current
+    @time_card = TimeCard.new
+  end
+
+  def create
+    @time_card = TimeCard.new(time_card_params)
+    if @time_card.save
+      render :index
+    else
+      redirct_to  new_time_card_path
+    end
+  end
+
+  private
+  def time_card_params
+    params.require(:time_card).permit(:year, :month, :day, :in, :out).merge(user_id: current_user.id)
+>>>>>>> Stashed changes
   end
 end
--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,2 +1,26 @@
 class TimeCard < ApplicationRecord
+<<<<<<< Updated upstream
+=======
+  belongs_to :user
+
+  with_options presence: true do 
+    validates :year
+    validates :month
+    validates :in
+    validates :out
+  end
+
+  def working_status
+    case [!!:in, !!:out]
+      when [false, false]
+        :not_arrived # 未出社
+      when [true, false]
+        :working # 勤務中
+      when [true, true]
+        :left # 退社済
+    end
+  end
+
+
+>>>>>>> Stashed changes
 end
