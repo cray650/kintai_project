fc42f70581cb67ab382104b472f77f7605753049
cray650 <69844416+cray650@users.noreply.github.com>
2020-10-29 21:33:29 +0900

    管理者機能実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  employee_id: "999",
+  firstname:  "管理者",
+  lastname: "太郎",
+  email: "admin@example.jp",
+  password:  "1111aa",
+  password_confirmation: "1111aa",
+  admin: true)
