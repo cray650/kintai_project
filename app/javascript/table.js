29f7efc01648a632d0c7cb7ac55a50bce4d1ad84
cray650 <69844416+cray650@users.noreply.github.com>
2020-10-29 23:12:50 +0900

    new_viewの時計_修正

--- app/javascript/table.js
+++ app/javascript/table.js
@@ -1,4 +1,9 @@
-window.addEventListener('load', function(){
-  const submit = document.getElementById("in-submit")
-
-});
+function clock() {
+  var nowTime = new Date();
+  var nowHour = nowTime.getHours();
+  var nowMin  = nowTime.getMinutes();
+  var nowSec  = nowTime.getSeconds();
+  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("RealtimeClockArea").innerHTML = msg;
+}
+setInterval(clock,1000);
