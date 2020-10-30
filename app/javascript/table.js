0ad05eaa14200e8589fd962dbe256866a8d89c5f
cray650 <69844416+cray650@users.noreply.github.com>
2020-10-30 15:34:13 +0900

    管理者ユーザー実装

--- app/javascript/table.js
+++ app/javascript/table.js
@@ -1,9 +1,9 @@
 function clock() {
-  var nowTime = new Date();
-  var nowHour = nowTime.getHours();
-  var nowMin  = nowTime.getMinutes();
-  var nowSec  = nowTime.getSeconds();
-  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  const nowTime = new Date();
+  const nowHour = nowTime.getHours();
+  const nowMin  = nowTime.getMinutes();
+  const nowSec  = nowTime.getSeconds();
+  const msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
   document.getElementById("RealtimeClockArea").innerHTML = msg;
 }
 setInterval(clock,1000);
