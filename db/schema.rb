cb16aa11fcee7ad329681b580d53fd2eaf2f2e8a
cray650 <69844416+cray650@users.noreply.github.com>
2020-11-04 19:17:24 +0900

    ユーザー一覧機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_111428) do
+ActiveRecord::Schema.define(version: 2020_11_03_090302) do
 
   create_table "requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "type_id", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_111428) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
