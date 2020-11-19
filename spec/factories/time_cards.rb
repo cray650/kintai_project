b8cbf814e6730b28961f6a51f0fb3794eef69c6b
cray650 <69844416+cray650@users.noreply.github.com>
2020-11-19 13:50:15 +0900

    requestのテストコード実装

--- spec/factories/time_cards.rb
+++ spec/factories/time_cards.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :time_card do
-    year    { '2020'}
+    year    { '2020' }
     month   {'11'}
     day     {'11'}
     #in      {Faker::Time.forward(days: 23, period: :morning) }
     out     {Faker::Time.forward(days: 23, period: :morning) }
-    user_id {'999'}
+    association :user
   end
 end
