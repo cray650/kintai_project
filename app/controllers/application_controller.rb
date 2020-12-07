6f221d0bc84fff0220a52da122e45ed5d550cd2a
cray650 <69844416+cray650@users.noreply.github.com>
2020-12-07 18:36:03 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[employee_id firstname lastname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[employee_id firstname lastname salary])
   end
 end
